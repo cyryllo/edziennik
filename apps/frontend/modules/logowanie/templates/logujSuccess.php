92f8b8e00c5e2589cf97092798dbc4c339e09658
Cyryl Sochacki <cyrylsochacki@gmail.com>
2011-12-11 00:03:44 +0100

    włączony mod-rewrite i po problemie

--- apps/frontend/modules/logowanie/templates/logujSuccess.php
+++ apps/frontend/modules/logowanie/templates/logujSuccess.php
@@ -1,6 +1,6 @@
 <div id="logowanie"><h2>Logowanie</h2>
 <center>
-<form action="<?php echo '/index.php'.url_for('logowanie/weryfikuj') ?>" method="post">
+<form action="<?php echo url_for('logowanie/weryfikuj') ?>" method="post">
  <table>
     <?php echo $form ?>
     <tr>
