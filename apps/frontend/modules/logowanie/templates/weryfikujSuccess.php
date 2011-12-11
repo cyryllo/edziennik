67095bc47b1daee47c2ba87fac1a08f3332530e6
Cyryl Sochacki <cyrylsochacki@gmail.com>
2011-12-11 14:39:04 +0100

    wstępna praca z logowaniem

--- apps/frontend/modules/logowanie/templates/weryfikujSuccess.php
+++ apps/frontend/modules/logowanie/templates/weryfikujSuccess.php
@@ -1,10 +1,30 @@
 <?php
 $login= $sf_params->get('login');
 $haslo= $sf_params->get('haslo');
-$haslo=sha1($haslo);
+//$haslo=sha1($haslo);
 
 
  if ($login && $haslo) {
+ 	
+$c = new Criteria();
+$c->add(UzytkownikPeer::LOGIN, $login);
+$c->add(UzytkownikPeer::HASLO, sha1($haslo));
+$u = UzytkownikPeer::doSelectOne($c);
+	
+if($u)
+{
+	echo "zalogowano";
+	$this->getUser()->setAuthenticated(true);
+    $this->getUser()->addCredentials($u->getRodzaj());
+	
+	
+	
+	
+}else{
+	echo "Nie zalogowano";
+	
+}	
+	
 ?>
 <ul>
   <li>Login:    <?php echo $login ?></li>
