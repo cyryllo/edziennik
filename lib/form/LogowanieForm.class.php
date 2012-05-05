b16372f6b4291e115cd16dbc12d8f222054b9f04
Cyryl Sochacki <cyrylsochacki@gmail.com>
2012-05-05 13:05:45 +0200

    formularz logowania

--- lib/form/LogowanieForm.class.php
+++ lib/form/LogowanieForm.class.php
@@ -4,8 +4,8 @@ class LogowanieForm extends BaseForm
   public function configure()
   {
     $this->setWidgets(array(
-      'login'    => new sfWidgetFormInputText(array('label' => 'Login')),
-      'haslo'   => new sfWidgetFormInputPassword(array('label' => 'Hasło'))
+      'login'    => new sfWidgetFormInputText(array('label' => 'Login'),array('size' => 14)),
+      'haslo'   => new sfWidgetFormInputPassword(array('label' => 'Hasło'),array('size' => 14))
       ));
 
   }
