345e8da34e61b5e3880bd4f36629168633748dd2
Cyryl Sochacki <cyrylsochacki@gmail.com>
2012-05-14 14:04:20 +0200

    t

--- apps/backend/modules/zaplecze/templates/adminSuccess.php
+++ apps/backend/modules/zaplecze/templates/adminSuccess.php
@@ -1,12 +1,12 @@
 <div id="menu">
 	
-	 <a href="<?php echo url_for('zaplecze/users') ?>"><img src="/images/uzytkownicy.png" alt="Użytkownicy" /></a>
+	 <a href="<?php echo url_for('zaplecze/users') ?>" title="Zarządzaj użytkownikami"><img src="/images/uzytkownicy.png" alt="Użytkownicy" /></a>
 	 <a href="<?php echo url_for('zaplecze/tablicaocen') ?>"><img src="/images/tablicaocen.png" alt="Tablica ocen" /></a>
 	 <a href="<?php echo url_for('zaplecze/grupy') ?>"><img src="/images/grupy.png" alt="Grupy" /></a>
 	 <a href="<?php echo url_for('zaplecze/klasy') ?>"><img src="/images/klasy.png" alt="Klasy" /></a>
 	 <a href="<?php echo url_for('zaplecze/planlekcji') ?>"><img src="/images/planlekcji.png" alt="Plan lekcji" /></a>
 	 <a href="<?php echo url_for('zaplecze/semestry') ?>"><img src="/images/semestry.png" alt="Semestry" /></a> .:.
-	 <a href="<?php echo url_for('zaplecze/wyloguj') ?>"><img src="/images/wyloguj.png" alt="Wyloguj" /></a>
+	 <a href="<?php echo url_for('zaplecze/wyloguj') ?>" title="Wyloguj się z dziennika"><img src="/images/wyloguj.png" alt="Wyloguj" /></a>
 	 
  
 </div>
