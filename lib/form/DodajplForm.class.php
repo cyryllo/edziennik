f8c706b69db62ca97143c0272c3ae32bdaf2d455
Cyryl Sochacki <cyrylsochacki@gmail.com>
2012-05-16 10:28:04 +0200

    pl

--- lib/form/DodajplForm.class.php
+++ lib/form/DodajplForm.class.php
@@ -4,6 +4,8 @@ class DodajplForm extends BaseForm
   protected static $semestry = array('1' => 'I/2011/2012', '2' => 'II/2011/2012');
   protected static $przedmioty = array('Język Polski' => 'Język Polski', 'Matematyka' => 'Matematyka');
   protected static $dnityg = array('Poniedziałek','Wtorek', 'Środa', 'Czwartek', 'Piątek');
+   protected static $grupy = array('brak','I grupa', 'II grupa');
+   protected static $obowiazek = array('1' => 'Tak','0' =>'Nie');
   public function configure()
   {
     $this->setWidgets(array(
@@ -14,6 +16,9 @@ class DodajplForm extends BaseForm
       'czas_start'	=> new sfWidgetFormTime(array( 'format' => '%hour%:%minute%', 'label' => 'Od godziny')),
       'czas_stop'	=> new sfWidgetFormTime(array( 'format' => '%hour%:%minute%', 'label' => 'Do godziny')),
       'dzien_tygodnia'	=> new sfWidgetFormSelect(array('choices' => self::$dnityg), array('label' => 'Dzień tyg.')),
+      'godzina_lekcyjna'    => new sfWidgetFormInputText(array('label' => 'Godzina lekcyjna'),array('size' => 1)),
+      'grupa' => new sfWidgetFormSelect(array('choices' => self::$grupy), array('label' => 'Grupa')),
+      'obowiazkowa' => new sfWidgetFormSelect(array('choices' => self::$obowiazek), array('label' => 'Lekcja obowiązkowa')),
       ));
 
   }
