cad13f6e11579302fe136d927c48a2ddd881ccb9
Cyryl Sochacki <cyrylsochacki@gmail.com>
2012-06-24 16:40:02 +0200

    usunięcie wszstkiego

--- apps/backend/modules/zaplecze/templates/mkontoSuccess.php
+++
@@ -1,9 +0,0 @@
-<div id="menu">
-	
-	 
-	 <a href="<?php echo url_for('zaplecze/admin') ?>" title="Wróć do menu głównego" ><img src="/images/powrot.png" alt="Powrót" /></a>
-	 <a href="<?php echo url_for('zaplecze/mkonto') ?>" title="Moje konto"><img src="/images/mkonto.png" alt="Moje konto" /></a>
-	 <a href="<?php echo url_for('zaplecze/wyloguj') ?>" title="Wyloguj się z dziennika" ><img src="/images/wyloguj.png" alt="Wyloguj" /></a>
-	 
- 
-</div>
