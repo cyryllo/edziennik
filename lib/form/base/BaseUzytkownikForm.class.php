1656d9a84f9fa51ea1c6ec5ba80408604fccf79c
Cyryl Sochacki <cyrylsochacki@gmail.com>
2012-06-29 12:29:37 +0200

    edycja formularza + poprawa walidacji

--- lib/form/base/BaseUzytkownikForm.class.php
+++ lib/form/base/BaseUzytkownikForm.class.php
@@ -11,18 +11,21 @@
  */
 abstract class BaseUzytkownikForm extends BaseFormPropel
 {
+	protected static $rodzaje = array('u' => 'Uczeń', 'r' => 'Rodzic', 'n' => 'Nauczyciel', 's' => 'Sekretarka', 'a' => 'Administrator');
+	protected static $klasy = array('0' => 'Brak', '1' => '1E');
+	
   public function setup()
   {
     $this->setWidgets(array(
       'id'             => new sfWidgetFormInputHidden(),
-      'rodzaj'         => new sfWidgetFormInputText(),
+      'rodzaj'         => new sfWidgetFormSelect(array('choices' => self::$rodzaje), array('label' => 'Rodzaj')),
       'imie'           => new sfWidgetFormInputText(),
       'nazwisko'       => new sfWidgetFormInputText(),
       'login'          => new sfWidgetFormInputText(),
-      'haslo'          => new sfWidgetFormInputText(),
+      'haslo'          => new sfWidgetFormInputPassword(array('label' => 'Hasło'),array('size' => 14)),
       'email'          => new sfWidgetFormInputText(),
       'telefon'        => new sfWidgetFormInputText(),
-      'klasa_id'       => new sfWidgetFormInputText(),
+      'klasa_id'       => new sfWidgetFormSelect(array('choices' => self::$klasy, 'label' => 'Klasa')),
       'pesel'          => new sfWidgetFormInputText(),
       'dataurodzin'    => new sfWidgetFormDate(),
       'miejsceurodzin' => new sfWidgetFormInputText(),
@@ -31,7 +34,7 @@ abstract class BaseUzytkownikForm extends BaseFormPropel
       'ulica'          => new sfWidgetFormInputText(),
       'nrdomu'         => new sfWidgetFormInputText(),
       'kodpocztowy'    => new sfWidgetFormInputText(),
-      'panstwo'        => new sfWidgetFormInputText(),
+      'panstwo'        => new sfWidgetFormI18nChoiceCountry(array('culture' => 'pl','add_empty' => true,'default' => 'PL', 'label' => 'Państwo')),
       'info'           => new sfWidgetFormTextarea(),
       'aktywny'        => new sfWidgetFormInputCheckbox(),
     ));
@@ -46,7 +49,7 @@ abstract class BaseUzytkownikForm extends BaseFormPropel
       'email'          => new sfValidatorString(array('required' => false)),
       'telefon'        => new sfValidatorString(array('required' => false)),
       'klasa_id'       => new sfValidatorInteger(array('min' => -2147483648, 'max' => 2147483647, 'required' => false)),
-      'pesel'          => new sfValidatorInteger(array('min' => -2147483648, 'max' => 2147483647, 'required' => false)),
+      'pesel'          => new sfValidatorInteger(array('min' => 10000000000, 'max' => 99999999999, 'required' => false)),
       'dataurodzin'    => new sfValidatorDate(array('required' => false)),
       'miejsceurodzin' => new sfValidatorString(array('required' => false)),
       'ojciec'         => new sfValidatorInteger(array('min' => -2147483648, 'max' => 2147483647, 'required' => false)),
